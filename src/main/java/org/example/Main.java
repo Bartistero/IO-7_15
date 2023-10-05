ba6e5684fa3a4c8bae4674b7cff877dedb28cafc
Bartistero <bartek114@autograf.pl>
2023-10-05 18:33:24 +0200

    added new function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void cosTam(String name){
+        System.out.println(name);
+    }
 }
