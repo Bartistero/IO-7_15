ca9111686aacd4ee2483f5fb02811d9797639854
Bartistero <bartek114@autograf.pl>
2023-10-05 19:19:04 +0200

    Feat/pollub-2

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Customer {
+
+    public void newCustomer(String name) {
+        System.out.println(name);
+    }
+
+    public void deleteCustomer(String name) {
+        System.out.println(name);
+    }
+}
